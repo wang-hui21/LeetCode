9fe660f48bd82f3288b36fc5269fb59706fc6cd5
wang-hui21 <1024099045@qq.com>
2024-08-13 21:52:12 +0800

    算法

--- src/leetcode/editor/cn/T31_NextPermutation .java
+++ src/leetcode/editor/cn/T31_NextPermutation .java
@@ -60,11 +60,40 @@ public class T31_NextPermutation{
         // TO TEST
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public void nextPermutation(int[] nums) {
-        int n=0;
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            int i = nums.length - 2;
+            while(i >= 0 && nums[i] >= nums[i+1]){
+                i--;
+            }
+            if(i >= 0){
+                int j = nums.length - 1;
+                while(j>= 0 && nums[j] <= nums[i]){
+                    j--;
+                }
+                swap(nums,i,j);
+            }
+            reverse(nums,i+1);
+        }
+
+        public void swap(int[] nums,int i,int j){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+
+        //翻转从start开始的数组序列，原来肯定是降序（从大到小），转换为升序（从小到大）
+        public void reverse(int[] nums,int start){
+            int left = start;
+            int right = nums.length-1;
+            while(left < right){
+                swap(nums,left,right);
+                left++;
+                right--;
+            }
+        }
     }
-}
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
